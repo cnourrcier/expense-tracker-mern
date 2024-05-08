ea566c4119888e91108d75b95dc461b8e23e0521
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-07 21:00:15 -0500

    Create DB model, routes, controllers

---
+++ server.js
@@ -0,0 +1,23 @@
+// Backend uses commonJS module syntax
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const transactions = require('./routes/transactionsRoutes');
+
+const app = express();
+
+// Allows the use of req body parser
+app.use(express.json());
+
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
