29be87fd2eb2068b0d97345fffde060aa7e7c4f5
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-09 19:03:09 -0500

    Create build.sh file for full stack deployment

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the client directory
+cd client
+
+# Install dependencies
+npm install
+
+# Build the client
+npm run build
+
+# Navigate back to the root directory
+cd ..
